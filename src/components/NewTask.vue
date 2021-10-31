d554b7f9038a45e5fa1ecabd2ed0b0ace1ec6ce5
Alisson Davis <alissoneatena1@gmail.com>
2021-10-30 23:35:08 -0300

    Primeiro Commit

---
+++ src/components/NewTask.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="new-task">
+      <input type="text" v-model="texto" @keypress.enter="add" class="form-element" placeholder="Digite uma tarefa">
+      <button class="btn btn-primary form-element" @click="add" >+</button>
+      
+  </div>
+</template>
+
+<script>
+export default {
+   
+    data() {
+        return {
+            texto : ""
+        }
+    },
+    methods:{
+        add(){
+            this.$emit("TaskAdd",{name: this.texto})
+            this.texto = ""
+        }
+    }
+}
+</script>
+
+<style>
+    .new-task{
+        display: flex;
+        margin: 35px;
+    }
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border:1px solid #FFF;
+        padding: 5px 10px 8px;
+        color: #FFF;
+    }
+    input.form-element{
+        width: 500px;
+        background-color: #FFF2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+    button.form-element{
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-left: none;
+    }
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tasks">
+      
+      <template v-if="tasks.length">
+          <Task v-for="task in tasks" :key="task.name" :task="task">
+           
+          </Task>
+      </template>
+      <template v-else>
+          
+          <p>Sua vida está em dia!</p>    
+        
+      </template>
+  </div>
+</template>
+
+<script>
+import Task from './Taks.vue'
+export default {
+    components:{Task},
+    props:{
+        tasks : {type:Array, require:true}
+    }
+}
+</script>
+
+<style>
+    .tasks{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+</style>
